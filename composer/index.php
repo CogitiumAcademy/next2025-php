5d23824636c85e9c448af2861429342a90e22bf7
PhG <info@cogitium.com>
2025-02-25 12:22:10 +0100

    Jour 6 : Cours Composer jusqu'au slug

---
+++ composer/index.php
@@ -0,0 +1,55 @@
+<?php
+// /index.php
+
+// Chargement de l'autoloading
+require 'vendor/autoload.php';
+
+// Lien vers la classe utilisée
+// Le require sera fait par l'autoloading
+use App\classes\Member;
+use Cocur\Slugify\Slugify;
+use Ausi\SlugGenerator\SlugGenerator;
+
+// Instanciation
+$member = new Member("John", "Doe");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test de l'autoloading PSR-4</title>
+</head>
+
+<body>
+
+    <h1>Page de test</h1>
+
+    <h2>Fiche détaillée d'un membre :</h2>
+    
+    <dl>
+        <dt>Prénom</dt>
+        <dd><?= $member->getFirstName() ?></dd>
+        <dt>Nom</dt>
+        <dd><?= $member->getLastName() ?></dd>
+        <dt>Date</dt>
+        <dd><?= $member->getDate()->format('Y-m-d H:i:s') ?></dd>
+    </dl>
+
+    <h2>Display d'un membre : <?= $member->printMember() ?></h2>
+
+    <h2>Test du slug par bundle externe</h2>
+    <?php
+        $slugify = new Slugify();
+        echo $slugify->slugify($member->getFirstName() . ' ' . $member->getLastName() . ' ' . $member->getDate()->format('Y-m-d H:i:s')); 
+    ?>
+
+    <h2>Test du slug par bundle externe bis</h2>
+    <?php
+        $generator = new SlugGenerator;
+        echo $generator->generate('Hello Wörld!');  // Output: hello-world
+    ?>
+</body>
+</html>
---
+++ composer/src/classes/Member.php
@@ -0,0 +1,38 @@
+<?php
+// src/classes/Member.php
+
+// Définition de l'espace de nom de la classe
+namespace App\classes;
+
+// Définition de la classe, le nom doit correspondre au nom du fichier
+class Member {
+  
+    // Les propriétés
+    private $firstName;
+    private $lastName;
+    private $date;
+
+    // Le constructeur
+    function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->date = new \DateTime();
+    }
+
+    // les setters / getters
+    public function getFirstName() {
+        return $this->firstName;
+    }
+    public function getLastName() {
+        return $this->lastName;
+    }
+    public function getDate() {
+        return $this->date;
+    }
+
+    // les autres méthodes
+    public function printMember() {
+        echo $this->date->format('Y-m-d') . ' = ' . $this->firstName . ' ' . $this->lastName;
+    }
+
+}
